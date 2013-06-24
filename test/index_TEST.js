27e6cd2562bc511cfdca1007f625ef35ae06d003
asciidisco <public@asciidisco.com>
2013-06-24 17:03:08 +0200

    Mocha boilerplate

---
+++ test/index_TEST.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+describe('dalek-driver-native', function() {
+
+  it('i should think of proper testing', function(){
+    expect('native').to.equal('native');
+  });
+
+});

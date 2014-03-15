ab4b0d052d146c357b89a3d43527feeb4b36d6e3
mdix <dix@mediaman.de>
2014-03-15 22:02:47 +0100

    Refactored tests a bit

--- test/lib_commands_screenshot_TEST.js
+++ test/lib_commands_screenshot_TEST.js
@@ -4,7 +4,7 @@ var expect = require('chai').expect;
 var Screenshot = require('../lib/commands/screenshot.js');
 
 describe('dalek-driver-native Commands Screenshot', function() {
-  var Fakedate = function(timeString) {
+  var MockDate = function(timeString) {
     this.getHours = function() {
       return timeString.hours;
     };
@@ -48,32 +48,37 @@ describe('dalek-driver-native Commands Screenshot', function() {
 
   describe('_parseDateTime', function() {
     it('should return the ISO 8601 formatted date time for non UTC dates with negative offset hours (YYYY-MM-DDThh:mm:ss-hh:mm)', function() {
-      var dateTime = screenshot._parseDatetime(new Fakedate({hours: 10, minutes: 20, seconds: 30, timezoneOffset: -60}));
+      var dateTime = screenshot._parseDatetime(new MockDate({hours: 10, minutes: 20, seconds: 30, timezoneOffset: -60}));
       expect(dateTime).to.match(/^[\d]{4}-[\d]{2}-[\d]{2}T{1}[\d]{2}:[\d]{2}:[\d]{2}(-|\+)[\d]{2}:[\d]{2}$/);
     });
     it('should return the ISO 8601 formatted date time for non UTC dates with negative offset minutes (YYYY-MM-DDThh:mm:ss-hh:mm)', function() {
-      var dateTime = screenshot._parseDatetime(new Fakedate({hours: 10, minutes: 20, seconds: 30, timezoneOffset: -30}));
+      var dateTime = screenshot._parseDatetime(new MockDate({hours: 10, minutes: 20, seconds: 30, timezoneOffset: -30}));
       expect(dateTime).to.match(/^[\d]{4}-[\d]{2}-[\d]{2}T{1}[\d]{2}:[\d]{2}:[\d]{2}(-|\+)[\d]{2}:[\d]{2}$/);
     });
     it('should return the ISO 8601 formatted date time for non UTC dates with negative offset hours and minutes (YYYY-MM-DDThh:mm:ss-hh:mm)', function() {
-      var dateTime = screenshot._parseDatetime(new Fakedate({hours: 10, minutes: 20, seconds: 30, timezoneOffset: -90}));
+      var dateTime = screenshot._parseDatetime(new MockDate({hours: 10, minutes: 20, seconds: 30, timezoneOffset: -90}));
       expect(dateTime).to.match(/^[\d]{4}-[\d]{2}-[\d]{2}T{1}[\d]{2}:[\d]{2}:[\d]{2}(-|\+)[\d]{2}:[\d]{2}$/);
     });
     it('should return the ISO 8601 formatted date time for non UTC dates with positive offset hours (YYYY-MM-DDThh:mm:ss+hh:mm)', function() {
-      var dateTime = screenshot._parseDatetime(new Fakedate({hours: 10, minutes: 20, seconds: 30, timezoneOffset: +60}));
+      var dateTime = screenshot._parseDatetime(new MockDate({hours: 10, minutes: 20, seconds: 30, timezoneOffset: +60}));
       expect(dateTime).to.match(/^[\d]{4}-[\d]{2}-[\d]{2}T{1}[\d]{2}:[\d]{2}:[\d]{2}(-|\+)[\d]{2}:[\d]{2}$/);
     });
     it('should return the ISO 8601 formatted date time for non UTC dates with positive offset minutes (YYYY-MM-DDThh:mm:ss+hh:mm)', function() {
-      var dateTime = screenshot._parseDatetime(new Fakedate({hours: 10, minutes: 20, seconds: 30, timezoneOffset: +20}));
+      var dateTime = screenshot._parseDatetime(new MockDate({hours: 10, minutes: 20, seconds: 30, timezoneOffset: +20}));
       expect(dateTime).to.match(/^[\d]{4}-[\d]{2}-[\d]{2}T{1}[\d]{2}:[\d]{2}:[\d]{2}(-|\+)[\d]{2}:[\d]{2}$/);
     });
     it('should return the ISO 8601 formatted date time for non UTC dates with positive offset hours and minutes (YYYY-MM-DDThh:mm:ss-hh:mm)', function() {
-      var dateTime = screenshot._parseDatetime(new Fakedate({hours: 10, minutes: 20, seconds: 30, timezoneOffset: 90}));
+      var dateTime = screenshot._parseDatetime(new MockDate({hours: 10, minutes: 20, seconds: 30, timezoneOffset: 90}));
       expect(dateTime).to.match(/^[\d]{4}-[\d]{2}-[\d]{2}T{1}[\d]{2}:[\d]{2}:[\d]{2}(-|\+)[\d]{2}:[\d]{2}$/);
     });
     it('should return the ISO 8601 formatted date time for UTC dates (YYYY-MM-DDThh:mm:ssZ)', function() {
-      var dateTime = screenshot._parseDatetime(new Fakedate({hours: 10, minutes: 20, seconds: 30, timezoneOffset: 0}));
+      var dateTime = screenshot._parseDatetime(new MockDate({hours: 10, minutes: 20, seconds: 30, timezoneOffset: 0}));
       expect(dateTime).to.match(/^[\d]{4}-[\d]{2}-[\d]{2}T{1}[\d]{2}:[\d]{2}:[\d]{2}Z$/);
+    });
+    // nyone in UTC +/-0 tell me if this works for you! :)
+    it('should use the current date if no parameter has been given and return the ISO 8601 formatted date time', function() {
+      var dateTime = screenshot._parseDatetime();
+      expect(dateTime).to.match(/[\d]{4}-[\d]{2}-[\d]{2}T{1}[\d]{2}:[\d]{2}:[\d]{2}(Z|(-|\+)[\d]{2}:[\d]{2})/);
     });
   });
 });

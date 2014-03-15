ae225b23d28a2c408f8107ce6f14a99297637bda
mdix <dix@mediaman.de>
2014-03-15 21:41:38 +0100

    Hardened regexps

--- test/lib_commands_screenshot_TEST.js
+++ test/lib_commands_screenshot_TEST.js
@@ -49,23 +49,23 @@ describe('dalek-driver-native Commands Screenshot', function() {
   describe('_parseDateTime', function() {
     it('should return the ISO 8601 formatted date time for non UTC dates with negative offset hours (YYYY-MM-DDThh:mm:ss-hh:mm)', function() {
       var dateTime = screenshot._parseDatetime(new Fakedate({hours: 10, minutes: 20, seconds: 30, timezoneOffset: -60}));
-      expect(dateTime).to.match(/[\d]{4}-[\d]{2}-[\d]{2}T{1}[\d]{2}:[\d]{2}:[\d]{2}(-|\+)[\d]{2}:[\d]{2}/);
+      expect(dateTime).to.match(/^[\d]{4}-[\d]{2}-[\d]{2}T{1}[\d]{2}:[\d]{2}:[\d]{2}(-|\+)[\d]{2}:[\d]{2}$/);
     });
     it('should return the ISO 8601 formatted date time for non UTC dates with negative offset hours and minutes (YYYY-MM-DDThh:mm:ss-hh:mm)', function() {
       var dateTime = screenshot._parseDatetime(new Fakedate({hours: 10, minutes: 20, seconds: 30, timezoneOffset: -90}));
-      expect(dateTime).to.match(/[\d]{4}-[\d]{2}-[\d]{2}T{1}[\d]{2}:[\d]{2}:[\d]{2}(-|\+)[\d]{2}:[\d]{2}/);
+      expect(dateTime).to.match(/^[\d]{4}-[\d]{2}-[\d]{2}T{1}[\d]{2}:[\d]{2}:[\d]{2}(-|\+)[\d]{2}:[\d]{2}$/);
     });
     it('should return the ISO 8601 formatted date time for non UTC dates with positive offset hours (YYYY-MM-DDThh:mm:ss+hh:mm)', function() {
       var dateTime = screenshot._parseDatetime(new Fakedate({hours: 10, minutes: 20, seconds: 30, timezoneOffset: +60}));
-      expect(dateTime).to.match(/[\d]{4}-[\d]{2}-[\d]{2}T{1}[\d]{2}:[\d]{2}:[\d]{2}(-|\+)[\d]{2}:[\d]{2}/);
+      expect(dateTime).to.match(/^[\d]{4}-[\d]{2}-[\d]{2}T{1}[\d]{2}:[\d]{2}:[\d]{2}(-|\+)[\d]{2}:[\d]{2}$/);
     });
     it('should return the ISO 8601 formatted date time for non UTC dates with positive offset hours and minutes (YYYY-MM-DDThh:mm:ss-hh:mm)', function() {
       var dateTime = screenshot._parseDatetime(new Fakedate({hours: 10, minutes: 20, seconds: 30, timezoneOffset: 90}));
-      expect(dateTime).to.match(/[\d]{4}-[\d]{2}-[\d]{2}T{1}[\d]{2}:[\d]{2}:[\d]{2}(-|\+)[\d]{2}:[\d]{2}/);
+      expect(dateTime).to.match(/^[\d]{4}-[\d]{2}-[\d]{2}T{1}[\d]{2}:[\d]{2}:[\d]{2}(-|\+)[\d]{2}:[\d]{2}$/);
     });
     it('should return the ISO 8601 formatted date time for UTC dates (YYYY-MM-DDThh:mm:ss.sTZD)', function() {
       var dateTime = screenshot._parseDatetime(new Fakedate({hours: 10, minutes: 20, seconds: 30, timezoneOffset: 0}));
-      expect(dateTime).to.match(/[\d]{4}-[\d]{2}-[\d]{2}T{1}[\d]{2}:[\d]{2}:[\d]{2}Z$/);
+      expect(dateTime).to.match(/^[\d]{4}-[\d]{2}-[\d]{2}T{1}[\d]{2}:[\d]{2}:[\d]{2}Z$/);
     });
   });
 });
